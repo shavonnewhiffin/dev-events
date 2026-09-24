5a2c2789d0b8cdabf68c09fc2b4e5b701e9fd963
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-09-23 19:18:11 -0700

    implement database models

---
+++ database/index.ts
@@ -0,0 +1,5 @@
+export { default as Event } from './event.model';
+export { default as Booking } from './booking.model';
+
+export type { IEvent, EventDocument } from './event.model';
+export type { IBooking, BookingDocument } from './booking.model';
